dbd4f33ddbb6759bbb1ee373d5418a3eac2acfcf
Sam Calvert <sam.clvrt@gmail.com>
2014-07-20 17:12:25 -0700

    add game fixtures and adapter

--- app/models/game.js
+++ app/models/game.js
@@ -5,4 +5,11 @@ var Game = DS.Model.extend({
   startTime: DS.attr('date'),
 });
 
+Game.reopenClass({
+  FIXTURES: [
+    {id: 1, name: 'first game'},
+    {id: 2, name: 'game the second'}
+  ]
+});
+
 export default Game;
--- app/routes/games.js
+++ app/routes/games.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return [{name: 'first game'}, {name: 'second game'}];
+    return this.store.find('game');
   }
 });
