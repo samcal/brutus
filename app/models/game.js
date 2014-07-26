71a0c7697eef880904b85723f414ab37b2043355
Sam Calvert <sam.clvrt@gmail.com>
2014-07-25 23:41:28 -0700

    add hasMany between users and games

--- app/models/game.js
+++ app/models/game.js
@@ -3,12 +3,13 @@ import DS from 'ember-data';
 var Game = DS.Model.extend({
   name: DS.attr('string'),
   startTime: DS.attr('date'),
+  players: DS.hasMany('user')
 });
 
 Game.reopenClass({
   FIXTURES: [
-    {id: 1, name: 'first game'},
-    {id: 2, name: 'game the second'}
+    {id: 1, name: 'first game', players: [1, 2]},
+    {id: 2, name: 'game the second', players: [1, 2, 3]}
   ]
 });
 
---
+++ app/models/user.js
@@ -0,0 +1,28 @@
+import DS from 'ember-data';
+
+var User = DS.Model.extend({
+  name: DS.attr('string'),
+  games: DS.hasMany('game')
+});
+
+User.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      name: 'brutus',
+      games: [1, 2]
+    },
+    {
+      id: 2,
+      name: 'oswald',
+      games: [1, 2]
+    },
+    {
+      id: 3,
+      name: 'booth',
+      games: [2]
+    }
+  ]
+});
+
+export default User;
